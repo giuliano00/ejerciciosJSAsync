5fb813a8ad58511758e7f56cbda98f380dfb3074
giuliano00 <giuliano.botto@davinci.edu.ar>
2020-09-13 17:08:31 -0300

    1

--- ejercicio_1/app.js
+++ ejercicio_1/app.js
@@ -3,9 +3,11 @@ const obtenerChiste = require("./library");
 
 // Codigo funcion callback
 
+Chiste = (element) => element.forEach(element => {
+    console.log(element.setup + element.punchline);
+})
 
-//csd
 // Fin codigo
 
 // Usar la funcion obtenerChiste(funcionCallback) en donde funcionCallback es una función que recibe el objeto chiste extraido
-obtenerChiste();
+obtenerChiste(Chiste);
